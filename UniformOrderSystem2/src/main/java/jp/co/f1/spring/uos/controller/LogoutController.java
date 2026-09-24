7e63d9f0e265456328a0cc56545f62d5f603f79d
runa kanazawa <runa@gmail.com>
2026-09-24 14:23:03 +0900

    login,logoutのcontroller Guestは仮

--- UniformOrderSystem2/src/main/java/jp/co/f1/spring/uos/controller/LogoutController.java
+++ UniformOrderSystem2/src/main/java/jp/co/f1/spring/uos/controller/LogoutController.java
@@ -3,21 +3,27 @@ package jp.co.f1.spring.uos.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LogoutController {
+	
+	//セッション使用
+		@Autowired
+		private HttpSession session;
 
-    @PostMapping("/logout")
-    public String logout(HttpServletRequest request) {
+	@PostMapping("/logout")
+    public ModelAndView logoutForm(ModelAndView mav,HttpServletRequest request) {
 
-        HttpSession session = request.getSession(false);
+    	//セッション情報をクリアする
+    	session.invalidate();
 
-        if (session != null) {
-            session.invalidate();
-        }
-
-        return "redirect:/login";
-    }
+    	//リダイレクト先を指定
+    	mav = new ModelAndView("redirect:/login");
+    	// ModelとView情報を返す
+    	return mav;
+	}
 }
