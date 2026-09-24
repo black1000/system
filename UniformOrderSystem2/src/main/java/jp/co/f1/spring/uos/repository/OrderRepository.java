b2b442ac926d2b419ffb32afceeb96c7985f0054
Ryo Sato <ryo.sato@5degrees.co.jp>
2026-09-24 17:47:22 +0900

    OrderRepository.javaを修正

--- UniformOrderSystem2/src/main/java/jp/co/f1/spring/uos/repository/OrderRepository.java
+++ UniformOrderSystem2/src/main/java/jp/co/f1/spring/uos/repository/OrderRepository.java
@@ -8,7 +8,7 @@ import jp.co.f1.spring.uos.entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
-	public Iterable<Order> findByUserid(String userid);
+	public Iterable<Order> findByOrderno(String orderno);
 
 	public int getQuantity();
 }
