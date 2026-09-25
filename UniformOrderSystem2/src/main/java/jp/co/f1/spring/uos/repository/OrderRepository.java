5d15120b0d9067c048806a698df6717eec1f9966
runa kanazawa <runa@gmail.com>
2026-09-25 11:42:12 +0900

    a

--- UniformOrderSystem2/src/main/java/jp/co/f1/spring/uos/repository/OrderRepository.java
+++ UniformOrderSystem2/src/main/java/jp/co/f1/spring/uos/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package jp.co.f1.spring.uos.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,8 @@ import jp.co.f1.spring.uos.entity.Order;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, String> {
 	public Iterable<Order> findByOrderno(String orderno);
+
+	public Optional<Order> findByIsbn(String orderno);
 
 	
 }
